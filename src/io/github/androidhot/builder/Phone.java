a9a33e9683a6ff22f6e16675a8dc34584b909f8a
AndroidHot <zhiwei.dev@gmail.com>
2018-04-06 20:53:54 +0800

    OverView:Implement Builder pattern.
    Description:
        new file:   src/io/github/androidhot/builder/Phone.java

---
+++ src/io/github/androidhot/builder/Phone.java
@@ -0,0 +1,65 @@
+package io.github.androidhot.builder;
+
+public class Phone {
+  // Required parameters
+  private final String mName;
+  private final float mPrice;
+
+  // Optional parameters
+  private final String mCpu;
+  private final int mMemory;
+  private final int mStorage;
+  private final float mWeight;
+
+  private Phone(Builder builder) {
+    mName = builder.name;
+    mPrice = builder.price;
+    mCpu = builder.cpu;
+    mMemory = builder.memory;
+    mStorage = builder.storage;
+    mWeight = builder.weight;
+  }
+
+  // some function
+
+  public static class Builder {
+    // Required parameters
+    private final String name;
+    private final float price;
+
+    // Optional parameters, initialized to default values.
+    private String cpu = "";
+    private int memory = 0;
+    private int storage = 0;
+    private float weight = 0;
+
+    public Builder(String name, float price) {
+      this.name = name;
+      this.price = price;
+    }
+
+    public Builder setCpu(String cpu) {
+      this.cpu = cpu;
+      return this;
+    }
+
+    public Builder setMemory(int memory) {
+      this.memory = memory;
+      return this;
+    }
+
+    public Builder setStorage(int storage) {
+      this.storage = storage;
+      return this;
+    }
+
+    public Builder setWeight(float weight) {
+      this.weight = weight;
+      return this;
+    }
+
+    public Phone build() {
+      return new Phone(this);
+    }
+  }
+}
